c3a050395e9400cc5c6dc2ad638818b1461d4231
Roy Cruz <rrcruz4@up.edu.ph>
2019-09-03 23:00:06 +0800

    Create project

---
+++ Places/Places/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Places
+//
+//  Created by Roi Cruz on 03/09/2019.
+//  Copyright © 2019 personal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
